610b491ae1e6dfb1e0e2d29339b3417479fc4999
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-05-15 18:50:01 +0400

    implement code based router

--- src/routes/(hidden-folder)/first-level.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(hidden-folder)/first-level')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/(hidden-folder)/first-level"!</div>
-}
